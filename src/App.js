b4dad244bcdc65d556f19642f7128a424b7cd2a5
nahom-23 <noahbirhanu23@gmail.com>
2024-05-04 16:56:14 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Offers from "./pages/Offers";
+import ForgotPassword from "./pages/ForgotPassword";
+import SignIn from "./pages/Signin";
+import SignUp from "./pages/Signup";
 function App() {
   return (
     <>
-      <h1 className="text-2xl bg-blue-400">hello man</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+        </Routes>
+      </Router>
     </>
   );
 }
