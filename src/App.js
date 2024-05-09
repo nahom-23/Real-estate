6a4d97a02280df1a3d7576e8a4b0839c35812c67
nahom-23 <noahbirhanu23@gmail.com>
2024-05-09 12:38:14 +0300

    signin and login page ui

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 import Home from "./pages/Home";
 import Offers from "./pages/Offers";
 import ForgotPassword from "./pages/ForgotPassword";
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Router>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/offers" element={<Offers />} />
